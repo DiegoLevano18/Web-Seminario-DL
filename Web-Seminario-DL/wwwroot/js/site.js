0034e9557f82494dcee6079dafd6f60d85be0d92
Diego Levano <diegolevanososa@gmail.com>
2024-04-04 05:49:17 -0500

    Terminado pagina estatica

--- Web-Seminario-DL/wwwroot/js/site.js
+++ Web-Seminario-DL/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+window.addEventListener('scroll', function () {
+    var menu = document.getElementById('tecsa-menu-1');
+    var scrollPosition = window.scrollY;
+
+    if (scrollPosition > 100) { // Cambia 100 por la posición en la que quieres que el menú se haga sticky
+        menu.classList.add('menu-sticky');
+    } else {
+        menu.classList.remove('menu-sticky');
+        console.log("ta q funciona :u")
+    }
+    
+});
